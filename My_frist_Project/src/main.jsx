ecc07a56da933400f20235e5ad5fce10ff6197bc
Yashwanth Bodapati <yashwanth.bodapati@gmail.com>
2025-04-16 11:00:19 -0500

    Adding react projects to dev

---
+++ My_frist_Project/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App,{App2} from './App'
+
+createRoot(document.getElementById('root')).render(
+<> 
+  <App2/> 
+ <App/></>
+
+)
